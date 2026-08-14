71dc6801f0539ade13129d4cca413d25373ea31d
shantokarmoker8 <shantokarmoker8@gmail.com>
2026-08-14 20:27:00 +0600

    restart

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h2 className="text-2xl text-red-500 font-bold">hello world</h2>
+    </>
+  )
+}
+
+export default App
